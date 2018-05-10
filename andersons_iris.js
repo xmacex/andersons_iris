e775985d6a6e9b3dce452f38e355d4016bdeba37
Mace Ojala <mace@kirjakaapeli.lib.hel.fi>
2018-05-10 14:25:16 +0200

    Changed to getting D3 from CDN, and also using fetchy d3.json()

--- andersons_iris.js
+++ andersons_iris.js
@@ -5,7 +5,14 @@ const sh = 100; // specimen height
 const sc = 7; // specimen columns
 var svg = undefined;
 
-d3.json('iris.json', iris => {
+window.onload = () => work();
+
+function work() {
+    console.log("plotting");
+    d3.json('iris.json').then(iris => anderson_plot(iris))
+}
+
+function anderson_plot(iris) {
     console.log(iris);
 
     x = d3.scaleLinear()
@@ -14,7 +21,7 @@ d3.json('iris.json', iris => {
 
     y = d3.scaleLinear()
 	.domain([0, 8])
-	.range([0, sh])
+	.range([0, sh]);
 
     svg = d3.select('#viz').append('svg')
 	.attr('width', sw * iris.length)
@@ -26,20 +33,18 @@ d3.json('iris.json', iris => {
 	.append('rect')
 	.attr('class', 'sepal')
 	.attr('x', (d, i) => ((sw / 2) + (i % sc) * x.range()[1] + (sw / 2) - (x(d['sepal width (cm)']) / 2)))
-    // .attr('y', (d, i) => sh * Math.floor(i / sc))
 	.attr('y', (d, i) => sh * Math.floor(i / sc) - y(d['sepal length (cm)']))
 	.attr('width', d => x(d['sepal width (cm)']))
 	.attr('height', d => y(d['sepal length (cm)']))
 	.style('opacity', 0.5);
-    
+
     svg.selectAll('rect.petal')
 	.data(iris)
 	.enter()
 	.append('rect')
 	.attr('class', 'petal')
 	.attr('x', (d, i) => ((sw / 2) + (i % sc) * x.range()[1] + (sw / 2) - (x(d['petal width (cm)']) / 2)))
-    	.attr('y', (d, i) => sh * Math.floor(i / sc) - y(d['petal length (cm)']))
+	.attr('y', (d, i) => sh * Math.floor(i / sc) - y(d['petal length (cm)']))
 	.attr('width', d => x(d['petal width (cm)']))
-	.attr('height', d => y(d['petal length (cm)']))
-
-});
+	.attr('height', d => y(d['petal length (cm)']));
+}
